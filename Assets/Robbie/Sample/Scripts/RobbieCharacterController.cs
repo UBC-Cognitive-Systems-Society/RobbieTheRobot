3f3500ad2b0aba2c90d777819ce490fc9f2399a7
abelw <unoctium1@gmail.com>
2020-12-06 05:03:44 -0800

    Initial project set up

---
+++ Assets/Robbie/Sample/Scripts/RobbieCharacterController.cs
@@ -0,0 +1,49 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobbieCharacterController : MonoBehaviour
+{
+
+    protected Animator anim;
+    [SerializeField] protected Camera cam;
+    [SerializeField] protected private float waveTime;
+
+    public bool allowMovement = true;
+
+    public CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    protected IEnumerator TriggerWave()
+    {
+        anim.SetBool("Wave", true);
+        yield return new WaitForSeconds(waveTime);
+        anim.SetBool("Wave", false);
+    }
+
+    public void FadeOutAudio(AudioSource source, float t)
+    {
+        StartCoroutine(FadeOutSound(source, t));
+    }
+
+    static IEnumerator FadeOutSound(AudioSource source, float time)
+    {
+        float startVolume = source.volume;
+
+        while (source.volume > 0)
+        {
+            source.volume -= startVolume * Time.deltaTime / time;
+
+            yield return null;
+        }
+        source.Stop();
+        source.volume = startVolume;
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Robbie The Robot
+
+Simple model of [Robbie The Non-Binary Robot](http://www.cogsubc.ca/exec-team.html), the UBC COGS Mascot
+
+Includes an fbx of Robbie, as well as prefabs for use in Unity projects, and a very simple character controller. The project defaults to an animated face with Unity UI, if you want to use this outside of Unity make sure to grab [RobbieFace-NotAnimated.psd](link tbd)
+
+Robbie is rigged with [Mixamo](https://www.mixamo.com), so he should be compatible with any animations from there. Note that since he has some extra bones, you need to first upload the fbx in order to have animations rigged correctly.
+
+The project uses skyboxes from [Fantasy Skyboxes by Render Knight](https://assetstore.unity.com/packages/2d/textures-materials/sky/fantasy-skybox-18216) and Robbie's antenna is designed with [Dynamic Bone by Will Hong](https://assetstore.unity.com/packages/tools/animation/dynamic-bone-16743) in mind.
+
+See [here](link tbd) to preview Robbie in different lighting
